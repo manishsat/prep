9bddbb16d6009952c63d68d9412c08a4b459e3d5
Manish Satwani <masatwan@microsoft.com>
2024-09-08 18:37:49 -0700

    Update BinarySearch.java

--- preparation/src/my/prep/practice/binarysearch/BinarySearch.java
+++ preparation/src/my/prep/practice/binarysearch/BinarySearch.java
@@ -1,60 +1,73 @@
 package my.prep.practice.binarysearch;
 
 
-// you can also use imports, for example:
-// import java.util.*;
+/* You are given an m x n integer matrix matrix with the following two properties:
 
-//12 23 34 35 40
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
 
-//55 67 68 69 70
-
-//72 75 82 89 90
+You must write a solution in O(log(m * n)) time complexity.
+*/
 
 public class BinarySearch {
 
     public static void main(String [] args) {
-
         int[][] matrix = new int[][] {{12, 23, 34, 35 ,40}, {55, 67, 68, 69 ,70}, {72, 75, 82 ,89 ,90}};
-        Tuple t = findIndex(matrix, 104);
-        if(t != null) {
-            System.out.println(t.i);
-            System.out.println(t.j);
-        }else {
-            System.out.println("element does not exists");
-        }
-       
+        int target = 40;
+        boolean = result = searchMatrix(matrix, target);
+        Syste,.out.ptintln(result);   
     }
 
-    public static Tuple findIndex(int[][] matrix, int value) {
-        //assume matrix is sorted
-        int col = matrix[0].length -1;
+   public boolean searchMatrix(int[][] matrix, int target) {
+        // find a row
+        int rowIndex = findRow(0, matrix.length - 1, matrix[0].length - 1, matrix, target);
+        if(rowIndex == -1) {
+            return false;
+        }
 
-        for (int row =0 ; row < matrix.length; row++) {
-            if(matrix[row][0] < value && matrix[row][col] > value) {
-                // element is in this row
-                return binarySearch(row, col, matrix, value);
-            }
-        } // O (row) + log N
-        
-        return null;
+        return findElement(rowIndex, 0,  matrix[0].length - 1, matrix, target);
     }
 
-    public static Tuple binarySearch(int startIndex, int endIndex, int[][] matrix, int value) {
-        if(startIndex >= endIndex) {
-            return null;
+    public boolean findElement(int rowIndex, int startIndex, int endIndex, int[][] matrix, int target) 
+    {
+        if(startIndex > endIndex) {
+            return false;
         }
 
-        if(matrix[startIndex][endIndex] == value) {
-            return  new Tuple(startIndex,  endIndex);
+        int midIndex = startIndex + (endIndex - startIndex)/2;
+
+        if(matrix[rowIndex][midIndex] == target) {
+            return true;
         }
 
-        int midIndex =  startIndex + ((endIndex - startIndex)/2); //TODO
-        //int midIndex = (startIndex + endIndex )/2;
-        if(matrix[startIndex][midIndex] >= value) {
-            return binarySearch(startIndex, midIndex, matrix, value);
+        if(matrix[rowIndex][midIndex] < target) {
+            return findElement(rowIndex, midIndex + 1, endIndex, matrix, target);
         } else {
-            return binarySearch(midIndex, endIndex, matrix, value);
+            return findElement(rowIndex, startIndex, midIndex - 1, matrix, target);
         }
+
+    }
+    public int findRow(int firstRow, int lastRow, int lastColIndex, int[][] matrix, int target) {
+        
+        if(firstRow > lastRow) {
+            return -1;
+        }
+
+        int midRow = firstRow + (lastRow - firstRow)/2;
+        
+
+        if(matrix[midRow][0]<= target && matrix[midRow][lastColIndex] >= target) {
+            // row found
+            return midRow;
+        }
+
+        if(matrix[midRow][0] > target) {
+            return findRow(firstRow, midRow - 1, lastColIndex, matrix, target);
+        } else {
+            return findRow(midRow + 1, lastRow , lastColIndex, matrix, target);
+        }
+
     }
 }
 
